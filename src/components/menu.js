2f95ac09a95ceea846e33f8ecc459c11fd08d4dc
Your Name <you@example.com>
2023-05-01 13:21:27 +0530

    first commit

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
+import React,{useState,useEffect} from 'react';
+import Link from 'next/link';
 
 export const Menu=()=>{
+  const[menuclick ,setMenuclick] = useState('')
+
+  useEffect(()=>{
+      let pathName = window.location.pathname
+      setMenuclick(pathName)
+      setMenuclick(pathName.slice(1) || 'home')
+  },[])
+
+  const fnclick=(eve)=>{
+   setMenuclick(eve.target.id)
+  }
+
     return (
       <ul className='menu'>
       <li>
-        <Link href="/home">Home</Link>
+        <Link id='home' className={menuclick == 'home' && 'active-menu'} onClick={fnclick} href="/home">Home</Link>
       </li>
       <li>
-        <Link href="/about">About us</Link>
+        <Link  id='about' className={menuclick == 'about' && 'active-menu'} onClick={fnclick} href="/about">About us</Link>
       </li>
       <li>
-        <Link href="/contact">Contact</Link>
+        <Link id='contact' className={menuclick == 'contact' && 'active-menu'} onClick={fnclick} href="/contact">Contact</Link>
       </li>
     </ul>
     )
